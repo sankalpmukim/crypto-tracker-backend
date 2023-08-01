c66e8a94353fadc539562fc7c54443ad118ef010
Sankalp Mukim <sankalpmukim@gmail.com>
2023-08-02 03:23:59 +0530

    feat: set and select coins

--- src/controllers/coins.controller.ts
+++ src/controllers/coins.controller.ts
@@ -1,7 +1,56 @@
+import { CoinAssetType, Coin, SelectedCoin, Prisma } from "@prisma/client";
 import { prisma } from "../initializers/prisma";
-import fs from "fs/promises";
 
-export const getCoins = async () => {
+export const getCoins = async (): Promise<Coin[]> => {
   const coins = await prisma.coin.findMany();
   return coins;
+};
+
+export const selectCoin = ({
+  userId,
+  coin,
+}: {
+  userId: string;
+  coin: string;
+}): Promise<SelectedCoin> => {
+  return prisma.selectedCoin.create({
+    data: {
+      userId,
+      coinAssetType: coin as CoinAssetType,
+    },
+  });
+};
+
+export const setUserCoins = async ({
+  userId,
+  coins,
+}: {
+  userId: string;
+  coins: string[];
+}) => {
+  const updated = prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      selectedCoins: {
+        deleteMany: {
+          NOT: {
+            coinAssetType: {
+              in: coins as CoinAssetType[],
+            },
+          },
+        },
+        createMany: {
+          data: coins.map((coin) => ({
+            coinAssetType: coin as CoinAssetType,
+          })),
+        },
+      },
+    },
+    include: {
+      selectedCoins: true,
+    },
+  });
+  return updated;
 };
