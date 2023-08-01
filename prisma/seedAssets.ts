a2026da92414e8e4436b8b194774f7d71d51c61b
Sankalp Mukim <sankalpmukim@gmail.com>
2023-08-02 02:04:40 +0530

    feat: add select coins schema

---
+++ prisma/seedAssets.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+
+// read "metadata_new.json" file
+fs.readFile("./metadata_new.json", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // parse JSON object
+  const metadata = JSON.parse(data);
+
+  // log JSON object
+  // console.log();
+  const strData = Array.from(
+    new Set(metadata.map((m: any) => m.asset_id_base))
+  ).join("\n");
+
+  fs.writeFile("./assets.txt", strData, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    //file written successfully
+  });
+});
